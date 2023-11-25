0406e417be69194dae6de1159446409ba95560da
Toshi <huynhduykhoi619@gmail.com>
2023-11-25 20:13:39 +0700

    connecting to MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const db = "mongodb://127.0.0.1:27017/MERN_bookstore";
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, db } from "./config.js";
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    return res.status(200).send("Hello world");
+});
 
-
+// starting app
 app.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
 });
+
+// connecting to database
+mongoose.connect(db)
+    .then(() => console.log("Connect to database successfully"))
+    .catch((error) => console.log(error));
