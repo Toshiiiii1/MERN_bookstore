b8f5c2035203afda9d52b188bae348085dd73fbf
Toshi <huynhduykhoi619@gmail.com>
2023-11-26 15:37:08 +0700

    feat: show specific book info

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
                                     </td>
                                     <td className="border border-slate-700 rounded-md text-center">
                                         <div className="flex justify-center gap-x-4">
-                                            <Link to={`/books/detail/${book.id}`}>
+                                            <Link to={`/books/detail/${book._id}`}>
                                                 <BsInfoCircle className="text-2xl text-green-800"/>
                                             </Link>
                                             <Link to={`/books/edit/${book.id}`}>
