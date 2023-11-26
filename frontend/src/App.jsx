ac78d1282856b9aa420dac82fad9782dacaf298e
Toshi <huynhduykhoi619@gmail.com>
2023-11-26 13:34:32 +0700

    create components and define routes for it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateBook from "./components/CreateBook.jsx";
+import Home from "./components/Home.jsx";
+import DeleteBook from "./components/DeleteBook.jsx";
+import EditBook from "./components/EditBook.jsx";
+import ShowBook from "./components/ShowBook.jsx";
 
 const App = () => {
-  return (
-    <div className='bg-red-400 text-white'>App</div>
-  )
-}
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/books/create" element={<CreateBook />} />
+				<Route path="/books/detail/:id" element={<ShowBook />} />
+				<Route path="/books/edit/:id" element={<EditBook />} />
+				<Route path="/books/delete/:id" element={<DeleteBook />} />
+			</Routes>
+		</>
+	);
+};
 
-export default App
+export default App;
